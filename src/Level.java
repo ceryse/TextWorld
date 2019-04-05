4a820f935520c81d88b5b3d8dd76bcdc829ad1c7
ceryse <cerysedwards7@gmail.com>
2019-04-05 10:34:34 -0700

    level and main

---
+++ src/Level.java
@@ -0,0 +1,158 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Level {
+    private HashMap<String, Room> rooms;
+
+    public Level() {
+        rooms = new HashMap<String, Room>();
+    }
+
+    public Room getRandomRoom() {
+        ArrayList<Room> allRooms = new ArrayList<>(rooms.values());
+        return allRooms.get((int) (Math.random() * rooms.size()));
+    }
+
+    public void addRoom(String name, String description) {
+        rooms.put(name, new Room(name, description));
+    }
+
+    public void addDirectedEdge(String name1, String name2) {
+        Room n1 = rooms.get(name1);
+        Room n2 = rooms.get(name2);
+        n1.addNeighbor(n2);
+    }
+
+    public void addUndirectedEdge(String name1, String name2) {
+        addDirectedEdge(name1, name2);
+        addDirectedEdge(name2, name1);
+    }
+
+    public Room getRoom(String name) {
+        return rooms.get(name);
+    }
+
+    public class Room {
+        private String name;
+        private HashMap<String, Room> neighbors;
+        private String description;
+        private ArrayList<Item> items;
+        private ArrayList<Creature> creatures;
+
+        private Room(String name, String description) {
+            this.name = name;
+            this.description = description;
+            neighbors = new HashMap<String, Room>();
+            items = new ArrayList<>();
+            creatures = new ArrayList<>();
+        }
+
+        public ArrayList getCreatures() {
+            return creatures;
+        }
+
+        public void removeCreature(Creature c) {
+            creatures.remove(c);
+        }
+
+        public void addCreature(Creature c) {
+            creatures.add(c);
+        }
+
+        public boolean destroyItem(String name) {
+            for (Item item : items) {
+                if (item.getName().equals(name)) {
+                    items.remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Item removeItem(String name) {
+            for (Item item : items) {
+                if (item.getName().equals(name)) {
+                    items.remove(item);
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public void addItem(Item item) {
+            items.add(item);
+        }
+
+        public String displayItems() {
+            String output = "";
+            for (Item i : items) {
+                output += i.getName() + " ";
+            }
+            return output;
+        }
+
+        public ArrayList<Item> getItems() {
+            return items;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public Room getNeighbor(String name) {
+            return neighbors.get(name);
+        }
+
+        public String getNeighborNames() {
+            String output = "";
+            for (String name : neighbors.keySet()) {
+                output += name + " ";
+            }
+            return output;
+        }
+
+        private void addNeighbor(Room room) {
+            neighbors.put(room.getName(), room);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public HashMap<String, Room> getNeighbors() {
+            return neighbors;
+        }
+
+        public void setNeighbors(HashMap<String, Room> neighbors) {
+            this.neighbors = neighbors;
+        }
+
+        public String getCreatureNames() {
+            String output = "";
+            for (Creature c : creatures) {
+                if (c instanceof Chicken) {
+                    output += "Chicken ";
+                } else if (c instanceof Wumpus) {
+                    output += "Wumpus ";
+                } else if (c instanceof Popstar) {
+                    output += "Popstar ";
+                }
+            }
+            return output;
+        }
+
+        public Room getRandomNeighbor() {
+            ArrayList<Room> rooms = new ArrayList<Room>(neighbors.values());
+            return rooms.get((int) (Math.random() * rooms.size()));
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Level l = setUpLevel();
+        Player p = setUpPlayer(l);
+        ArrayList<Creature> creatures = setUpCreatures(l, p);
+
+        String response = "";
+        Scanner in = new Scanner(System.in);
+
+        displayOptions();
+        do {
+            System.out.println("You are in the " + p.getCurrentRoom().getName());
+            System.out.print("What do you want to do? >");
+            response = in.nextLine();
+            response.trim();
+            moveCreatures(creatures);
+            if (response.contains("go") && containsTwoBrackets(response)) {
+                response = getInnerString(response);
+                if (!p.moveRoom(l.getRoom(response))) {
+                    System.out.println("You can't go to " + response);
+                }
+            } else if (response.equals("look")) {
+                look(p);
+            } else if (response.contains("add room") && containsTwoBrackets(response)) {
+                System.out.print("Type a description >");
+                String description = in.nextLine();
+                l.addRoom(getInnerString(response), description);
+                l.addUndirectedEdge(p.getCurrentRoom().getName(), getInnerString(response));
+                p.setCurrentRoom(l.getRoom(getInnerString(response)));
+            } else if (response.contains("take") && containsTwoBrackets(response)) {
+                response = getInnerString(response);
+                Item temp = p.getCurrentRoom().removeItem(response);
+                if (temp == null) {
+                    System.out.println(response + " doesn't exist!");
+                } else {
+                    p.addItem(temp);
+                }
+            } else if (response.contains("drop") && containsTwoBrackets(response)) {
+                response = getInnerString(response);
+                Item temp = p.removeItem(response);
+                if (temp == null) {
+                    System.out.println("You do not have a " + response);
+                } else {
+                    p.getCurrentRoom().addItem(temp);
+                }
+            } else if (response.contains("inventory")) {
+                System.out.println(p.getInventory());
+            } else if (response.equals("quit")) {
+                continue;
+            } else {
+                displayOptions();
+            }
+
+        } while (!response.equals("quit"));
+    }
+
+    private static Player setUpPlayer(Level g) {
+        Player p = new Player("Cerys", "me");
+        p.setCurrentRoom(g.getRoom("hall"));
+        return p;
+    }
+
+    private static ArrayList<Creature> setUpCreatures(Level g, Player p) {
+        ArrayList<Creature> creatures = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            creatures.add(new Chicken(p, g.getRandomRoom()));
+        }
+
+        creatures.add(new Wumpus(p, g.getRandomRoom()));
+        creatures.add(new Popstar(p, g.getRandomRoom()));
+
+        return creatures;
+    }
+
+    private static Level setUpLevel() {
+        Level l = new Level();
+        l.addRoom("hall", "a long dark hallway");
+        l.addRoom("closet", "a dark, dark closet");
+        l.addRoom("dungeon", "a dank dark dungeon");
+        l.addRoom("bedroom", "a cozy bedroom");
+        l.addRoom("bathroom", "a cold, tiled bathroom");
+        l.addRoom("ladder", "a long ladder");
+
+        l.addUndirectedEdge("hall", "bedroom");
+        l.addUndirectedEdge("hall", "closet");
+        l.addUndirectedEdge("bedroom", "bathroom");
+        l.addDirectedEdge("ladder", "hall");
+        l.addUndirectedEdge("ladder", "dungeon");
+        l.addDirectedEdge("bathroom", "dungeon");
+
+        l.getRoom("hall").addItem(new Item("lobster", "a red lobster"));
+        l.getRoom("closet").addItem(new Item("duck", "a mallard"));
+        l.getRoom("bathroom").addItem(new Item("water bottle", "a green metallic water bottle"));
+
+        return l;
+    }
+
+    private static void moveCreatures(ArrayList<Creature> creatures) {
+        for (Creature c : creatures) {
+            c.move();
+        }
+    }
+
+    private static void displayOptions() {
+        System.out.println("Type 'go <roomname>' to go to the room\nType 'look' to look around the room\n" +
+                "Type 'add room <roomname>' to add a neighbor to your current room.\nType 'take <object>' to pick up an object" +
+                "\nType 'drop <object> to drop an object\nType 'inventory' to display your inventory\nType 'quit' to quit");
+    }
+
+    private static void look(Player p) {
+        System.out.println("You are in " + p.getCurrentRoom().getDescription());
+        System.out.println("You can go to the " + p.getCurrentRoom().getNeighborNames());
+        System.out.println("The items in this room are: " + p.getCurrentRoom().displayItems());
+        System.out.println("The creatures in this room are " + p.getCurrentRoom().getCreatureNames());
+    }
+
+    private static boolean containsTwoBrackets(String response) {
+        return response.indexOf("<") != -1 && response.indexOf(">") != -1 && response.indexOf("<") < response.indexOf(">");
+    }
+
+    private static String getInnerString(String response) {
+        return response.substring(response.indexOf("<") + 1, response.indexOf(">"));
+    }
+}
